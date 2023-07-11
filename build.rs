760695c217564b3d1cab5d92a11f24b88a422734
Christian <christian.belloni@babyloncloud.com>
2023-07-11 15:59:16 +0200

    implemented bessel_y wrapper

---
+++ build.rs
@@ -0,0 +1,41 @@
+use std::{path::PathBuf, process::Command};
+
+macro_rules! p {
+    ($($tokens: tt)*) => {
+        println!("cargo:warning={}", format!($($tokens)*))
+    }
+}
+
+pub fn main() {
+    _ = get_fortran_compiler().expect("gfortran-13 not installed!");
+    let out_path: PathBuf = std::env::var("OUT_DIR").unwrap().into();
+    let lib_name = "amos";
+    let mut lib_path = out_path.clone();
+    lib_path.push(format!("lib{}.a", lib_name));
+
+    let cmd = Command::new("gfortran-13")
+        .arg("-shared")
+        .arg("amos/amos_iso_c_fortran_wrapper.f90")
+        .arg("amos/machine.for")
+        .arg("amos/zbesh.for")
+        .arg("-o")
+        .arg(lib_path.to_string_lossy().as_ref())
+        .output()
+        .expect("failed to compile fortran library,\nare you sure you have gfortran-13 installed?");
+    let err = String::from_utf8_lossy(&cmd.stderr);
+    println!("cargo:rustc-link-search={}", out_path.to_string_lossy());
+    println!("cargo:rustc-link-lib={}", "amos");
+}
+
+fn get_fortran_compiler() -> Option<String> {
+    Some(
+        String::from_utf8_lossy(
+            &Command::new("which")
+                .arg("gfortran-13")
+                .output()
+                .ok()?
+                .stdout,
+        )
+        .to_string(),
+    )
+}
