f0d468fa178b1c8b5c0660cc0c1b924d2c78b7be
Christian <christian.belloni@babyloncloud.com>
2023-07-12 18:22:03 +0200

    fixed docs rs

--- build.rs
+++ build.rs
@@ -9,23 +9,28 @@ macro_rules! p {
 
 #[cfg(not(doc))]
 pub fn main() {
-    _ = get_fortran_compiler().expect("gfortran-13 not installed!");
-    let out_path: PathBuf = std::env::var("OUT_DIR").unwrap().into();
-    let lib_name = "amos";
-    let mut lib_path = out_path.clone();
-    lib_path.push(format!("lib{lib_name}.a"));
+    if std::env::var("DOCS_RS").is_ok() {
+    } else {
+        _ = get_fortran_compiler().expect("gfortran-13 not installed!");
+        let out_path: PathBuf = std::env::var("OUT_DIR").unwrap().into();
+        let lib_name = "amos";
+        let mut lib_path = out_path.clone();
+        lib_path.push(format!("lib{lib_name}.a"));
 
-    Command::new("gfortran-13")
-        .arg("-shared")
-        .arg("amos/amos_iso_c_fortran_wrapper.f90")
-        .arg("amos/machine.for")
-        .arg("amos/zbesh.for")
-        .arg("-o")
-        .arg(lib_path.to_string_lossy().as_ref())
-        .output()
-        .expect("failed to compile fortran library,\nare you sure you have gfortran-13 installed?");
-    println!("cargo:rustc-link-search={}", out_path.to_string_lossy());
-    println!("cargo:rustc-link-lib=amos");
+        Command::new("gfortran-13")
+            .arg("-shared")
+            .arg("amos/amos_iso_c_fortran_wrapper.f90")
+            .arg("amos/machine.for")
+            .arg("amos/zbesh.for")
+            .arg("-o")
+            .arg(lib_path.to_string_lossy().as_ref())
+            .output()
+            .expect(
+                "failed to compile fortran library,\nare you sure you have gfortran-13 installed?",
+            );
+        println!("cargo:rustc-link-search={}", out_path.to_string_lossy());
+        println!("cargo:rustc-link-lib=amos");
+    }
 }
 
 #[cfg(doc)]
